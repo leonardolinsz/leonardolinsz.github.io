3b1c83d06f6f29af79c5d235fc5b76f35f8cdddd
Daniel Tognelli Severo <danielsevero-ieg@instituto.ad>
2025-08-29 09:26:39 -0300

    finalizando o carrossel

--- js/script.js
+++ js/script.js
@@ -13,20 +13,6 @@ const reviews = [
     img: 'https://www.course-api.com/images/people/person-2.jpeg',
     text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
   },
-  {
-    id: 3,
-    name: 'peter jones',
-    job: 'intern',
-    img: 'https://www.course-api.com/images/people/person-4.jpeg',
-    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
-  },
-  {
-    id: 4,
-    name: 'bill anderson',
-    job: 'the boss',
-    img: 'https://www.course-api.com/images/people/person-3.jpeg',
-    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
-  },
 ];
 // select items
 const img = document.getElementById('person-img');
